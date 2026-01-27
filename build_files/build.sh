3e97b7c8dca0122adeadc6c3736dde815e3cbf35
Niklas <40402114+NiHaiden@users.noreply.github.com>
2026-01-27 19:43:01 +0100

    Fix symlink creation for 1password
    Remove existing symlink for 1password before creating a new one.

--- build_files/build.sh
+++ build_files/build.sh
@@ -84,6 +84,7 @@ rm /etc/yum.repos.d/1password.repo -f
 mv /var/opt/1Password /usr/lib/1Password # move this over here
 
 # Create a symlink /usr/bin/1password => /opt/1Password/1password
+rm /usr/bin/1password
 ln -s /opt/1Password/1password /usr/bin/1password
 
 #####
